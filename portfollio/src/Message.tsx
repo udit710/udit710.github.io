1414ecc4cd70a6ae54dac8cf77afe003cc159e33
udit710 <s3933905@student.rmit.edu.au>
2023-09-25 19:30:09 +1000

    Added framer, yet to use

--- portfollio/src/Message.tsx
+++ portfollio/src/Message.tsx
@@ -1,14 +1,24 @@
 import React, {Component} from "react";
 import "./Message.css"
+import { useState } from "react";
+import { motion, useViewportScroll, useTransform } from "framer-motion";
 
 export default class Message extends Component{
     render() {
+
+        const variable = ["Web Developer👨🏻‍💻","Backend Developer⚙️","App Developer📱","Soccer Enthusiast⚽️","Gym Rat💪🏻"];
+        var x = 0;
+        function increment(){
+            return x+=1;
+        }
+
+
         return (
             <div className="page">
-                <h1>__Hello__World__</h1>
+                <h1>Hi There! I am Udit Malshe</h1>
                 <body>
                     <div className="hi">
-                    Hi There
+                    AKA, <span className="title">{variable[x]}</span>
                     </div>
                 </body>
             </div>
