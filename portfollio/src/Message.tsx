14e241cfed1bbd0e96a47f5b9cecc657154b5f81
udit710 <s3933905@student.rmit.edu.au>
2023-09-25 19:01:22 +1000

    cleared css

---
+++ portfollio/src/Message.tsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import "./Message.css"
+
+export default class Message extends Component{
+    render() {
+        return (
+            <div className="page">
+                <h1>__Hello__World__</h1>
+                <body>
+                    <div className="hi">
+                    Hi There
+                    </div>
+                </body>
+            </div>
+        );
+    }
+}
