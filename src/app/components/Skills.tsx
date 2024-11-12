c583c094fc867a0e0a530432760bab19cad1dc7b
udit710 <s3933905@student.rmit.edu.au>
2024-11-12 16:55:56 +1100

    updated skills

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -9,42 +9,52 @@ function Skills() {
                 <h1 className="font-[Times] text-3xl mb-5">Stack</h1>
             <div className="md:flex md:flex-wrap">
                 <div className="box-content m-auto bg-white m4 px-8 py-2 text-blue mb-5 border-[0.5px] border-black shadow-lg hover:bg-black hover:text-white">
-                    <h1 className="text-3xl text-center font-[Times]">Web</h1>
+                    <h1 className="text-3xl text-center font-[Times]">Technologies</h1>
                     <p className="font-mono text-center">
-                        <Tech bg="blue" text="Node JS" /> <Tech bg="blue" text="React JS" /> <Tech bg="blue" text="Tailwind" /> <Tech bg="blue" text="Axios" /> <Tech bg="blue" text="Spring-Boot" /> <Tech bg="blue" text="REST Api" />
-                    {/* Node JS, React JS, Tailwind, Axios, Spring-Boot, REST Api */}
+                        <Tech bg="blue" text="Node JS" /> <Tech bg="blue" text="React JS" /> <Tech bg="blue" text="Tailwind" />  <Tech bg="blue" text="Flask" /> <Tech bg="blue" text="Django" /> <Tech bg="blue" text="Express" /> <Tech bg="blue" text="Next" /> <Tech bg="blue" text="Spring boot" /> 
                     </p>
-                    {/* <p className="flex items-center space-x-2">
-                        <FaNodeJs />Node JS, React JS, Tailwind, Axios, Spring-Boot, REST Api.
-                    </p> */}
 
                 </div>
                 <div className="box-content m-auto bg-blue bg-opacity-60 m4 px-8 py-2 text-white mb-5 border-[0.5px] border-black shadow-lg hover:bg-black hover:text-white">
                     <h1 className="text-3xl text-center font-[Times]">Programming</h1>
                     <p className="font-mono text-center">
-                        <Tech bg="blue" text="Python" /> <Tech bg="blue" text="Javascript" /> <Tech bg="blue" text="Java" />
-                    {/* Python, Javascript, Java */}
+                         <Tech bg="blue" text="Object Oriented" /> <Tech bg="blue" text="Functional" /> <Tech bg="blue" text="Scripting" />  <Tech bg="blue" text="REST API" />
+                    {/* Object Oriented, Functional, Scripting, REST API */}
                     </p>
                 </div>
                 <div className="box-content m-auto bg-white m4 px-8 py-2 text-blue mb-5 border-[0.5px] border-black shadow-lg hover:bg-black hover:text-white">
                     <h1 className="text-3xl text-center font-[Times]">Cloud</h1>
                     <p className="font-mono text-center">
-                        <Tech bg="blue" text="AWS" /> <Tech bg="blue" text="EC2" /> <Tech bg="blue" text="Beanstalk" /> <Tech bg="blue" text="DynamoDB" /> <Tech bg="blue" text="API Gateway" /> <Tech bg="blue" text="S3" /> <Tech bg="blue" text="Lambda" />
-                    {/* AWS (EC2, Beanstalk, DynamoDB, API Gateway, S3, Lambda) */}
+                        <Tech bg="blue" text="AWS"/>  <Tech bg="blue" text="Serverless" /> <Tech bg="blue" text="Azure" /> <Tech bg="blue" text="GCP" />
+                    {/* AWS, Serverless, Azure, GCP */}
                     </p>
                 </div>
                 <div className="box-content m-auto bg-blue bg-opacity-60 m4 px-8 py-2 text-white mb-5 border-[0.5px] border-black shadow-lg hover:bg-black hover:text-white">
                     <h1 className="text-3xl text-center font-[Times]">App</h1>
                     <p className="font-mono text-center">
-                        <Tech bg="blue" text="Swift" /> <Tech bg="blue" text="SwiftUI" /> <Tech bg="blue" text="UIKit" /> <Tech bg="blue" text="Core Data" />
-                    {/* SwiftUI, UIKit, Core Data */}
+                        <Tech bg="blue" text="SwiftUI" /> <Tech bg="blue" text="React Native" /> <Tech bg="blue" text="Flutter" /> <Tech bg="blue" text=".NET" /> <Tech bg="blue" text="SQL" />
+                        {/* SwiftUI, React Native, Flutter, .NET, SQL */}
                     </p>
                 </div>
                 <div className="box-content m-auto bg-white m4 px-8 py-2 text-blue mb-5 border-[0.5px] border-black shadow-lg hover:bg-black hover:text-white">
                     <h1 className="text-3xl text-center font-[Times]">CI/CD</h1>
                     <p className="font-mono text-center">
-                        <Tech bg="blue" text="Git" /> <Tech bg="blue" text="Docker" />
-                    {/* Git, Docker */}
+                        <Tech bg="blue" text="Git" /> <Tech bg="blue" text="Docker" /><Tech bg="blue" text="Github Actions" /> <Tech bg="blue" text="Linux" />
+                    { /* Git, Docker, Github Actions, Linux */}
+                    </p>
+                </div>
+                <div className="box-content m-auto bg-blue bg-opacity-60 m4 px-8 py-2 text-white mb-5 border-[0.5px] border-black shadow-lg hover:bg-black hover:text-white">
+                    <h1 className="text-3xl text-center font-[Times]">ML</h1>
+                    <p className="font-mono text-center">
+                        <Tech bg="blue" text="Tensorflow" /><Tech bg="blue" text="Pytorch" /><Tech bg="blue" text="NLP" /><Tech bg="blue" text="Pre-trained models" /><Tech bg="blue" text="CV" />
+                    {/* Tensorflow, Pytorch, NLP, Pre-trained models, CV */}
+                    </p>
+                </div>
+                <div className="box-content m-auto bg-white m4 px-8 py-2 text-blue mb-5 border-[0.5px] border-black shadow-lg hover:bg-black hover:text-white">
+                    <h1 className="text-3xl text-center font-[Times]">Others</h1>
+                    <p className="font-mono text-center">
+                        <Tech bg="blue" text="Leadership" /><Tech bg="blue" text="Communication" /><Tech bg="blue" text="Team Work" /><Tech bg="blue" text="Agile" />
+                    {/* Leadership, Communication, Team Work, Agile */}
                     </p>
                 </div>
             </div>
