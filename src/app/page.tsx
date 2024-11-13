0e85a75b2c8e5855727da35567225b2decf0278b
udit710 <s3933905@student.rmit.edu.au>
2024-11-13 12:57:22 +1100

    fixed extra scroll on small screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,8 @@ const Home: NextPage = () => {
       <About />
       <div className='flex max-h-[100vh] '>
         <Sidebar activeSection={activeSection} />
-        <main className="flex-1 p-10 lg:overflow-y-auto scroll-smooth overflow-x-hidden">
+        {/* <main className="flex-1 p-10 lg:overflow-y-auto scroll-smooth "> */}
+        <main className="flex-1 p-10 w-[100%] lg:overflow-y-auto scroll-smooth">
           <section id='about'>
             <Intro />
           </section>
