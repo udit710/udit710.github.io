38496b80c298c6e61e83998610eb64176a731ef7
udit710 <s3933905@student.rmit.edu.au>
2024-11-13 12:22:27 +1100

    fixed more

--- src/app/componentCards/SkillCategory.tsx
+++ src/app/componentCards/SkillCategory.tsx
@@ -12,11 +12,11 @@ const SkillCategory: React.FC<SkillCategoryProps> = ({ title, skills, bgColor })
     return (
         <div className={`box-content m-auto ${bgColor} px-8 py-2 mb-5 border-[0.5px] border-black shadow-lg hover:bg-black hover:text-white`}>
             <h1 className="text-3xl text-center font-[Times]">{title}</h1>
-            <p className="font-mono text-center">
+            <div className="font-mono text-center flex flex-wrap justify-center">
                 {skills.map((skill, index) => (
                     <Tech key={index} bg="blue" text={skill} />
                 ))}
-            </p>
+            </div>
         </div>
     );
 };
