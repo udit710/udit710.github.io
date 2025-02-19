6ec1fa4b6ddb15b62dc23c95cce504d676282451
udit710 <s3933905@student.rmit.edu.au>
2025-02-19 12:23:56 +1100

    Update Experience

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -17,7 +17,7 @@ function Experience() {
                     <li>Implemented Agile methodologies, including Scrum, to enhance collaboration, streamline project management, and deliver high-quality solutions on time.</li>
                     <li>Engaged with clients to understand requirements, provide Technical recommendations, and deliver customised solutions that meet their business needs.</li>
                     <br />
-                    <div className=" text-center text-sm font-mono"><Tech bg="blue" text="AWS" /> <Tech bg="blue" text="React JS" /> <Tech bg="blue" text="Django" /> </div>
+                    <div className=" text-center text-sm font-mono"><Tech bg="blue" text="AWS" /> <Tech bg="blue" text="Agile" /> <Tech bg="blue" text="REST API" /> </div>
                     </div>
                 </div>
                 <div className="box-content m-auto bg-blue bg-opacity-10 m4 text-black mb-5 border-[0.5px] border-black hover:shadow-xl ">
@@ -26,11 +26,11 @@ function Experience() {
                     <h2 className="font-[Times] text-xl">Apr &apos;24 - Present</h2>
                     </div>
                     <div className="font-mono text-left text-sm md:text-lg px-4 py-2">
-                    <li>Developed a website for the Cybersecurity club at RMIT.</li>
-                    <li>Collaborate with team members using version control systems such as Git to manage code changes efViciently.</li>
+                    <li>Led the migration to server-side rendering (SSR) frameworks, reducing data latency by 35%.</li>
+                    <li>Collaborate with team members using version control systems such as Git to manage code changes efficiently.</li>
                     <li>Employed frameworks to ensure the website&apos;s functionality, responsiveness, and security. Participate in regular meetings to discuss project progress, brainstorm ideas, and address any challenges that arise during development.</li>
                     <br />
-                    <div className=" text-center text-sm font-mono"><Tech bg="blue" text="Node JS" /> <Tech bg="blue" text="React JS" /> <Tech bg="blue" text="Bootstrap" /> </div>
+                    <div className=" text-center text-sm font-mono"><Tech bg="blue" text="Node JS" /> <Tech bg="blue" text="React JS" /> <Tech bg="blue" text="Git" /> </div>
                     </div>
                 </div>
             </div>
